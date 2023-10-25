7f9d48cc783c05f38c33e22db39924c7e8d3608f
pragath2003 <148952944+pragath2003@users.noreply.github.com>
2023-10-25 14:18:45 +0530

    Add files via upload

---
+++ src.js
@@ -0,0 +1,18 @@
+const insta = () => {
+
+    window.open("https://www.instagram.com/mr_420_pragath/" , '_blank');
+};
+
+const linkedin = () => {
+
+    window.open("https://www.linkedin.com/in/pragath-68",'_blank');
+};
+
+const twitter = () => {
+
+    window.open("https://twitter.com/mr_420_pragath" , '_blank');
+};
+
+const resume = (resume) => {
+    window.open(resume, "_blank");
+}
